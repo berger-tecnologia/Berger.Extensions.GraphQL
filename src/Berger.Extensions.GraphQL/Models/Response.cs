626e044d0c35905665731d330c0d3dcbdfd4791a
Rafael Cronemberger <rafael@berger.com.br>
2023-07-15 16:53:12 -0300

    Packet updates with build yamls

---
+++ src/Berger.Extensions.GraphQL/Models/Response.cs
@@ -0,0 +1,13 @@
+﻿using Berger.Extensions.Pagination;
+
+namespace Berger.Extensions.GraphQL
+{
+    public class Response<T> where T : class
+    {
+        #region Properties
+        public T First { get; set; } = Activator.CreateInstance<T>();
+        public List<T> List { get; set; } = new List<T>();
+        public Pagination<T> Pagination { get; set; } = new Pagination<T>();
+        #endregion
+    }
+}
